31515191662003196fed325240b5a162ab51f85c
Connie Standish <conniestand@gmail.com>
2022-05-28 14:36:37 -0400

    fixed array and started on for loop

--- index.js
+++ index.js
@@ -3,18 +3,33 @@
 var questionsArr = [
     {
         question: 'Loops allow you to write and execute code multiple times without repeating yourself',
-        answer: true,
+        answer: true
+    },
+    { 
         question: "An array is a list of data",
-        answer: true,
+        answer: true
+    },
+    {
         question: "In an object functions become known as methods",
-        answer: true,
+        answer: true
+    },
+    {
         question: "For loops is the most common loop",
-        answer: true,
+        answer: true
+    },
+    {
         question: "It is best practice to declare all global variables and functions first",
-        answer: true,
+        answer: true
+    },
+    {
         question: "Console.log is an example of an object and a method, because log is a method of console which is a window object.",
-        answer: true,
-    }
+        answer: true
+    },
 ]
+// console.log(questionsArr.length)
 
-function runQuiz() {}
+function runQuiz() {
+   for (var i = 0; i < questionsArr.length; i++) {
+    console.log(i) 
+   }  
+}
