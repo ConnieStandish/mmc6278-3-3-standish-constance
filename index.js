0078753e9fdbca14405b53c4815c6770f9bf8b56
Connie Standish <conniestand@gmail.com>
2022-05-28 13:08:57 -0400

    Created a global variable for the questions array and set up the start quiz function

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+
+var questionsArr = [
+    {
+        question: 'Loops allow you to write and execute code multiple times without repeating yourself',
+        answer: true,
+        question: "An array is a list of data",
+        answer: true,
+        question: "In an object functions become known as methods",
+        answer: true,
+        question: "For loops is the most common loop",
+        answer: true,
+        question: "It is best practice to declare all global variables and functions first",
+        answer: true,
+        question: "Console.log is an example of an object and a method, because log is a method of console which is a window object.",
+        answer: true,
+    }
+]
+
+function runQuiz() {}
